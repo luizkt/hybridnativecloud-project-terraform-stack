5f3e841367d72c202792ed37d7fbfa10cfc903c0
luizkt <keese.luiz@gmail.com>
2020-05-08 22:35:32 +0000

    Atualizado endereço do bucket

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-34scj-334018"
+    bucket = "hackathon-fiap-34scj-334018-lkt"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
